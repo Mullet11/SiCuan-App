0f8953fa3440d6c42ab4a97aeae42c9be6e13979
Mullet11 <rakha1140athallah@gmail.com>
2026-06-11 12:34:42 +0800

    Delete Modul1 directory

--- Modul1/app/src/main/java/com/example/praktikum_modul1_compose/DiceConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.example.praktikum_modul1_compose
-
-object DiceConfig {
-    const val SHUFFLE_DURATION_MILLIS = 500L
-    const val SHUFFLE_STEP_MILLIS = 50L
-    const val SHUFFLE_LOOP_COUNT = 10
-    const val ROTATION_INCREMENT = 360f
-}
